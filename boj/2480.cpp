209d9da94a241cc0f19971af44741feae83ce515
YeongIl Kim <duddlf1497@naver.com>
2024-03-24 21:16:11 +0900

    second commit

--- boj/2480.cpp
+++ boj/2480.cpp
@@ -9,14 +9,13 @@ int check(int a, int b, int c){
 int main(){
     int a,b,c;
     cin>>a>>b>>c;
-    check(a,b,c);
     int amount;
 
     if (a!=b && b!=c && c!=a) amount = check(a,b,c)*100;
-    else if (a==b && b==c) amount = 10000+(a*1000);
+    else if (a==b && b==c && c==a) amount = 10000+(a*1000);
     else{
         if (a==b) amount = (a*100) + 1000;
-        else (c*100)+1000;
+        else amount = (c*100)+1000;
     }
 
     cout<<amount;
