610d7e03727e47ba1ae6540ce4c03d364cc204de
KimYeong1 <164733879+KimYeong1@users.noreply.github.com>
2024-06-16 03:23:40 +0900

    Add files via upload

---
+++ boj/25314.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int input(){
+    int N;
+    cin>>N;
+    int num =N/4;
+    for (int i =0; i<num; i++){
+        cout<<"long ";
+    }
+    cout<<"int";
+    return 0;
+}
+
+
+int main(){
+    input();
+    return 0;
+}
